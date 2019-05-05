4276338113e84ed9527912dcfaf7471ca3f5e5ac
Drew Dahlman <info@drewdahlman.com>
2019-05-05 09:40:15 -0400

    adding ignore and editorconfig

--- wordpress/wp-content/themes/rip-it/footer.php
+++
@@ -1,8 +0,0 @@
-
-<script src="<?= get_template_directory_uri(); ?>/scripts/commons.js"></script>
-<script src="<?= get_template_directory_uri(); ?>/scripts/app.js"></script>
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
--- wordpress/wp-content/themes/rip-it/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Template Name: Index
-*/
-?>
-
-<?php get_header(); ?>
-Hello.
-<?php get_footer(); ?>
